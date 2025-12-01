79c91892311a5cfdd35a453b485fb416c0ea9ad4
Kwadwot <kwadwoptwumasi@gmail.com>
2025-12-01 03:46:48 -0500

    completed basic CRUD functions for server

--- server/queries.js
+++ server/queries.js
@@ -15,10 +15,17 @@ const pool = new POOL({
 //  requests and database queries ---
 
 // Create a new site
+const createSite = (request, response) => {
+    const { name, URL } = request.body;
+    pool.query('INSERT INTO sites (name, URL) VALUES ($1, $2)', [name, URL], (error, results) => {
+        if (error) {
+            throw error;
+        }
+        response.status(201).send(`Site added with ID: ${results.insertId}`);
+    });
+};
 
 // Read all sites
-
-// Get all sites
 const getSites = (request, response) => {
     pool.query('SELECT * FROM sites ORDER BY id ASC', (error, results) => {
         if (error) {
@@ -29,9 +36,32 @@ const getSites = (request, response) => {
 };
 
 // Update a site
+const updateSite = (request, response) => {
+    const { id } = request.params;
+    const { name, URL } = request.body;
+    pool.query('UPDATE sites SET name = $1, URL = $2 WHERE id = $3', [name, URL, id], (error) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Site updated with ID: ${id}`);
+    });
+};
+
 
 // Delete a site
+const deleteSite = (request, response) => {
+    const { id } = request.params;
+    pool.query('DELETE FROM sites WHERE id = $1', [id], (error) => {
+        if (error) {
+            throw error;
+        }
+        response.status(200).send(`Site deleted with ID: ${id}`);
+    });
+};
 
 module.exports = {
     getSites,
+    createSite,
+    updateSite,
+    deleteSite,
 };
