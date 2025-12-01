cf05b4b59664d19590087d64b7637c57972ac531
Kwadwot <kwadwoptwumasi@gmail.com>
2025-12-01 02:09:04 -0500

    installed necessary packages for server side and created gitignore

---
+++ seed.sql
@@ -0,0 +1,3 @@
+INSERT INTO sites (site_name, site_link, comic_type, popular_comic_name, popular_comic_link) VALUES
+('Read Comic Online', 'https://readcomiconline.li', 'Comic', 'Invincible (2003)', 'https://readcomiconline.li/Comic/Invincible'),
+('Asura Comic', 'https://asuracomic.net/', 'Manhwa', 'Eternally Regressing Knight', 'https://asuracomic.net/series/eternally-regressing-knight-c5ab4f7a');
